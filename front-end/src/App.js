3e4fe593acb4a66e6aaaf0d15551626bf3d2b08a
John Chamberlin <johnchamberlin00@gmail.com>
2021-01-29 18:42:59 -0600

    barebones links/routes added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,21 @@
 import Home from './components/Home'
+import {Link, Route}  from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div className='App'>
+      <div className='nav-bar'>
+        <Link to ='/'> Home</Link>
+        <Link to ='/login'>Login</Link>
+        <Link to ='/new-user'>Sign Up</Link>
+        <Link to ='/add-recipe'>Add Recipe</Link>
+
+        <Route path='/' component={Home}/>
+        {/* <Route path='/login' component={}/>
+        <Route path='/new-user' component={}/>
+        <Route path='/add-recipe' component={}/> */}
+      </div>
+    </div>
   )
 }
 
